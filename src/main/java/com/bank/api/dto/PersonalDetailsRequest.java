8c3b1dd552d8a4419fcb0c640ce1531d534b0458
subhadiprudra <neloy.nr2@gmail.com>
2023-10-02 20:19:31 +0530

    a

--- src/main/java/com/bank/api/dto/PersonalDetailsRequest.java
+++ src/main/java/com/bank/api/dto/PersonalDetailsRequest.java
@@ -17,7 +17,7 @@ public class PersonalDetailsRequest {
     @Size(min = 2, max = 50, message = "FirstName must be between 2 and 50 characters")
     String firstName;
 
-    @Size(min = 2, max = 50, message = "LastName must be between 2 and 50 characters")
+    @Size(min = 1, max = 50, message = "LastName must be between 1 and 50 characters")
     String lastName;
 
     @Size(min = 2, max = 50, message = "FatherName must be between 2 and 50 characters")
